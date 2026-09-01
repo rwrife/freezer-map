81e5b5e10e0fff3e9a91e5cfb86f84f8d5a2878e
rwrife <rwrife@users.noreply.github.com>
2026-09-01 18:24:19 +0000

    feat: build appliance zone and quick-add workflows

--- lib/presentation/freezer_map_app.dart
+++ lib/presentation/freezer_map_app.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:freezer_map/application/inventory_commands.dart';
+import 'package:freezer_map/domain/contracts.dart';
 import 'package:freezer_map/presentation/empty_inventory_screen.dart';
+import 'package:freezer_map/presentation/inventory_screen.dart';
 
 class FreezerMapApp extends StatelessWidget {
-  const FreezerMapApp({super.key});
+  const FreezerMapApp({super.key})
+    : repository = null,
+      clock = null,
+      ids = null;
+
+  const FreezerMapApp.inventory({
+    required TransactionalInventoryRepository this.repository,
+    required Clock this.clock,
+    required StableIdSource this.ids,
+    super.key,
+  });
+
+  final TransactionalInventoryRepository? repository;
+  final Clock? clock;
+  final StableIdSource? ids;
 
   static const appTitle = 'Freezer Map';
 
@@ -15,7 +32,16 @@ class FreezerMapApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF286983)),
         useMaterial3: true,
       ),
-      home: const EmptyInventoryScreen(),
+      home: repository == null
+          ? const EmptyInventoryScreen()
+          : InventoryScreen(
+              repository: repository!,
+              commands: InventoryCommands(
+                repository: repository!,
+                clock: clock!,
+                ids: ids!,
+              ),
+            ),
     );
   }
 }
